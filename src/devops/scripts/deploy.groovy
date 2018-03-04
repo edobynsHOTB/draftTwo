3465700f9d157bd922783d05004ec69ad71d568b
Eric Dobyns <edobyns@hotbsoftware.com>
2018-03-04 15:00:36 -0800

    fdsaasdf

--- src/devops/scripts/deploy.groovy
+++ src/devops/scripts/deploy.groovy
@@ -54,30 +54,36 @@ def publish() {
 def runStagingTests() {
     stage('Test') {
         sh '''#!/bin/bash
+            # Wait 10 seconds
             sleep 10
 
+            # Clean Up
             rm -rf /var/lib/jenkins/adpq_test_results
             mkdir /var/lib/jenkins/adpq_test_results
 
+            # Build & run container
             docker build ./src/qa -t adpq_tests
             docker run -v /var/lib/jenkins/adpq_test_results/reports:/data/reports -e Environment=staging --name adpq_tests -i adpq_tests >> /var/lib/jenkins/adpq_test_results/results.xml
             docker rm adpq_tests && docker rmi adpq_tests
 
+            # Extract test results and save to var RESULTS
             numberOfTests=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 2 | cut -d ' ' -f 1)
             errors=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 3 | cut -d ' ' -f 1)
             failures=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 4 | cut -d '>' -f 1)
             numberOfSuccesses=$(($numberOfTests - $errors - $failures))
-            echo "$numberOfSuccesses / $numberOfTests tests ran successfully" > testResults
+            echo "$numberOfSuccesses / $numberOfTests tests ran successfully" > RESULTS
 
+            # Create Shield.io badge url & set Slack result type
             testBadge=""
             if [ "$numberOfSuccesses" -lt "$numberOfTests" ]; then
-                echo "FAILURE" > resultType
+                echo "FAILURE" > RESULT_TYPE
                 testBadge=https://img.shields.io/badge/tests-$numberOfSuccesses%2F$numberOfTests-red.svg
             else
-                echo "SUCCESS" > resultType
+                echo "SUCCESS" > RESULT_TYPE
                 testBadge=https://img.shields.io/badge/tests-$numberOfSuccesses%2F$numberOfTests-brightgreen.svg
             fi
 
+            # Upload Image Badge to S3
             curl $testBadge >> ./testResultsImg.svg
             aws s3 cp --acl public-read ./testResultsImg.svg s3://adpq-assets/buildAssets/testResults.svg
             rm -rf ./testResultsImg.svg
