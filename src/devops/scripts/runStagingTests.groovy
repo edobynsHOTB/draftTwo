b6b6e812414a741a7d107bb66d0a116e13fe0603
Eric Dobyns <edobyns@hotbsoftware.com>
2018-03-04 17:12:19 -0800

    Final?

--- src/devops/scripts/runStagingTests.groovy
+++ src/devops/scripts/runStagingTests.groovy
@@ -50,9 +50,8 @@ def sendSlackNotification() {
     stage ('Notify') {
         def nodeHome = tool 'NodeTool' // Load Node.js
         env.PATH="${env.PATH}:${nodeHome}/bin" // Set Path
-        TIMESTAMP = sh(script: "echo \$(date +%s)", returnStdout: true)
         RESULTS = readFile 'RESULTS'
         RESULT_TYPE =  readFile 'RESULT_TYPE'
-        sh "sleep 10 && node ./src/devops/scripts/slackNotification.js \"$RESULT_TYPE\" \"*Nightly Test Results* - <!date^$TIMESTAMP^{date_long_pretty} {time}|April 20, 2018 4:20 PM>\" \"$RESULTS\""
+        sh "sleep 10 && node ./src/devops/scripts/slackNotification.js \"$RESULT_TYPE\" \"*Nightly Test Results*\" \"$RESULTS\""
     }
 }
